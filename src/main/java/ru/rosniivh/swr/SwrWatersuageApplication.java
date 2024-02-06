06ba4b09e96d0e40b00bbfd7158fe1e61d16dd4b
ershov <berettam92f@yandex.ru>
2024-02-06 10:41:19 +0300

    initial commit

---
+++ src/main/java/ru/rosniivh/swr/SwrWatersuageApplication.java
@@ -0,0 +1,13 @@
+package ru.rosniivh.swr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwrWatersuageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwrWatersuageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/rosniivh/swr/SwrWatersuageApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rosniivh.swr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwrWatersuageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
