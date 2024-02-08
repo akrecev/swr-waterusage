91023b2fce688eed135bdf6b22d11a1d09b9598b
RWEC\akrecev <akretsev@gmail.com>
2024-02-08 08:56:50 +0300

    add fd, rfs entities

---
+++ src/main/java/ru/rosniivh/swr/domain/catalog/CatFederalDistrict.java
@@ -0,0 +1,45 @@
+package ru.rosniivh.swr.domain.catalog;
+
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "cat_federal_district", schema = "dbo")
+public class CatFederalDistrict {
+    @Id
+    @Column(name = "uid", nullable = false)
+    private Integer id;
+
+    @Lob
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "fd")
+    private Set<CatRfSubject> catRfSubjects = new LinkedHashSet<>();
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        CatFederalDistrict that = (CatFederalDistrict) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
