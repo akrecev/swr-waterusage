a902f35985566cbac1e4c507f6e5442ae54f6b83
ershov <berettam92f@yandex.ru>
2024-02-08 16:05:56 +0300

    Добавил тестовый поиск по фильтру, по инн и наименованию в AsvImportLegalSubject

--- src/main/java/ru/rosniivh/swr/service/AsvImportLegalSubjectService.java
+++ src/main/java/ru/rosniivh/swr/service/AsvImportLegalSubjectService.java
@@ -1,5 +1,18 @@
 package ru.rosniivh.swr.service;
 
+import ru.rosniivh.swr.domain.object.asv.AsvImportLegalSubjectEntity;
+import ru.rosniivh.swr.dto.object.asv.AsvImportLegalSubjectDto;
+import ru.rosniivh.swr.filter.AsvImportLegalSubjectFilter;
+
+import java.util.List;
+
 public interface AsvImportLegalSubjectService {
 
+    List<AsvImportLegalSubjectDto> getByFilter(AsvImportLegalSubjectFilter filter);
+
+    AsvImportLegalSubjectEntity getById(Integer id);
+
+    List<AsvImportLegalSubjectDto> getByFullName(String fullName);
+
+    List<AsvImportLegalSubjectDto> getByInn(String inn);
 }
