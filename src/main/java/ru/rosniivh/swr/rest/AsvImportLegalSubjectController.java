2bed54a47f34c4073b56bf74d8b4912dad6de075
ershov <berettam92f@yandex.ru>
2024-02-08 16:47:27 +0300

    Add legalSubjectCheckJPQL

--- src/main/java/ru/rosniivh/swr/rest/AsvImportLegalSubjectController.java
+++ src/main/java/ru/rosniivh/swr/rest/AsvImportLegalSubjectController.java
@@ -65,4 +65,24 @@ public class AsvImportLegalSubjectController {
     public ResponseEntity<?> getByInn(@RequestParam String inn) {
         return ResponseEntity.ok(service.getByInn(inn));
     }
+
+    @GetMapping("/legalSubjectCheckJPQL")
+    public ResponseEntity<?> legalSubjectCheckJPQL(
+            @RequestParam(value = "type") Integer type,
+            @RequestParam(value = "inn") String inn,
+            @RequestParam(value = "name") String name,
+            @RequestParam(value = "ogrn", required = false) String ogrn,
+            @RequestParam(value = "okpo", required = false) String okpo,
+            @RequestParam(value = "okopf", required = false) String okopf,
+            @RequestParam(value = "okfs", required = false) String okfs,
+            @RequestParam(value = "okved", required = false) String okved,
+            @RequestParam(value = "okonh", required = false) String okonh,
+            @RequestParam(value = "postAddress", required = false) String postAddress,
+            @RequestParam(value = "legalAddress", required = false) String legalAddress,
+            @RequestParam(value = "okato", required = false) String okato
+    ) {
+
+        return ResponseEntity.ok().body(service.getByType(type, name, ogrn, okpo, postAddress,
+                legalAddress, okato, inn));
+    }
 }
