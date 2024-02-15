6350f6ceff28215fc583bb83b7e08acfaebf8be0
RWEC\tyabinskiy_anton <rwecrwec@rwec.ru>
2024-02-15 14:55:35 +0300

    3 more entities

---
+++ src/main/java/ru/rosniivh/swr/domain/object/WaterObjectEntity.java
@@ -0,0 +1,103 @@
+package ru.rosniivh.swr.domain.object;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "o_water_object", schema = "dbo")
+public class WaterObjectEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "uid", nullable = false)
+    private Integer id;
+
+    @Column(name = "code", nullable = false, length = Integer.MAX_VALUE)
+    private String code;
+
+    @Column(name = "category_id", nullable = false)
+    private Integer categoryId;
+
+    @Column(name = "type_id", nullable = false)
+    private Integer typeId;
+
+    @Column(name = "hep_id")
+    private Integer hepId;
+
+    @Column(name = "name", nullable = false, length = Integer.MAX_VALUE)
+    private String name;
+
+    @Column(name = "protected", nullable = false)
+    private Boolean protectedField = false;
+
+    @Column(name = "fishery_valuable", nullable = false)
+    private Boolean fisheryValuable = false;
+
+    @Column(name = "especially_fishery_valuable")
+    private Boolean especiallyFisheryValuable;
+
+    @Column(name = "shore_bias_deg")
+    private BigDecimal shoreBiasDeg;
+
+    @Column(name = "water_cadastre_number", length = Integer.MAX_VALUE)
+    private String waterCadastreNumber;
+
+    @Column(name = "water_cadastre_code", length = Integer.MAX_VALUE)
+    private String waterCadastreCode;
+
+    @Column(name = "estuary_distance")
+    private BigDecimal estuaryDistance;
+
+    @Column(name = "location_id")
+    private Integer locationId;
+
+    @Column(name = "cadastre_id")
+    private Integer cadastreId;
+
+    @Column(name = "fed_water_object_id")
+    private Integer fedWaterObjectId;
+
+    @Column(name = "owner_id")
+    private Integer ownerId;
+
+    @Column(name = "executive_authority_type_id")
+    private Integer executiveAuthorityTypeId;
+
+    @Column(name = "inserted_on", nullable = false)
+    private Instant insertedOn;
+
+    @Column(name = "inserted_by", nullable = false)
+    private Integer insertedBy;
+
+    @Column(name = "updated_on")
+    private Instant updatedOn;
+
+    @Column(name = "updated_by")
+    private Integer updatedBy;
+
+    @Column(name = "confirmed")
+    private Boolean confirmed;
+
+    @Column(name = "confirmed_on")
+    private Instant confirmedOn;
+
+    @Column(name = "confirmed_by")
+    private Integer confirmedBy;
+
+    @Column(name = "confirmation_document_id")
+    private Integer confirmationDocumentId;
+
+    @Column(name = "deleted")
+    private Boolean deleted;
+
+    @Column(name = "is2054")
+    private Boolean is2054;
+
+}
