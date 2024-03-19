b33ab03bf2bc09c5965a43c30089aa921db8edf1
RWEC\akrecev <akretsev@gmail.com>
2024-03-19 15:55:05 +0300

    delete security

--- src/main/java/ru/rosniivh/swr/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package ru.rosniivh.swr.service;
-
-import java.util.List;
-import ru.rosniivh.swr.domain.catalog.RfSubjectEntity;
-
-public interface UserService {
-    List<RfSubjectEntity> getUserRfs();
-}
