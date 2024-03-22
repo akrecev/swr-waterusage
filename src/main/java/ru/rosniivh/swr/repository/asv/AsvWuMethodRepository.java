85488b8d0cdca18733b78a5b16e94e5f80509f3c
RWEC\akrecev <akretsev@gmail.com>
2024-03-22 10:41:29 +0300

    fix AsvWuMethodRepository

--- src/main/java/ru/rosniivh/swr/repository/asv/AsvWuMethodRepository.java
+++ src/main/java/ru/rosniivh/swr/repository/asv/AsvWuMethodRepository.java
@@ -3,6 +3,7 @@ package ru.rosniivh.swr.repository.asv;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.rosniivh.swr.domain.catalog.asv.AsvWuMethodEntity;
+import ru.rosniivh.swr.domain.projections.AsvWuMethodsProjections;
 
-@RepositoryRestResource(path = "asvWuMethods", excerptProjection = AsvWuMethodEntity.class)
+@RepositoryRestResource(path = "asvWuMethods", excerptProjection = AsvWuMethodsProjections.class)
 public interface AsvWuMethodRepository extends CrudRepository<AsvWuMethodEntity, Integer> {}
